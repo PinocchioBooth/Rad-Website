390f3d7eb1c56e4579507183df1bee9c5dbd5e06
Jules Martinez <148609914+PinocchioBooth@users.noreply.github.com>
2025-02-21 10:53:22 -0500

    Add files via upload

---
+++ component/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function List() {
+  <div>
+    <ul>
+      <li>rad</li>
+      <li>rad</li>
+      <li>rad</li>
+    </ul>
+  </div>;
+}
---
+++ spellcheck.js
@@ -0,0 +1,2 @@
+<p contenteditable="true" spellcheck="true"> Do or do not, there is no
+  try </p>
